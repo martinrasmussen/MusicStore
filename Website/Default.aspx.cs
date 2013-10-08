aec970b3220c3de2aaec6c45ad9c59d25b3737a5
Antal J. Monori <anthonymonori@gmail.com>
2013-10-08 12:32:50 +0200

    Project file changed

--- Website/Default.aspx.cs
+++ Website/Default.aspx.cs
@@ -11,37 +11,38 @@ namespace Website
     {
         protected void Page_Load(object sender, EventArgs e)
         {
-        List<String> albumsList = new List<string>();
-        albumsList.Add("Hello Kitty");
-        albumsList.Add("GoGoGo");
-        albumsList.Add("One Direction");
-        albumsList.Add("Big Fat Snake");
+            List<String> albumsList = new List<string>();
+            albumsList.Add("Hello Kitty");
+            albumsList.Add("GoGoGo");
+            albumsList.Add("One Direction");
+            albumsList.Add("Big Fat Snake");
 
-        
-        foreach (string album in albumsList)
-        {
-            Image image = new Image();
-            image.ImageUrl = "~/resources/artwork/HotFuss.jpg";
-            //Initialize a row for the table
-            TableRow tblRow = new TableRow();
 
-            //Creates a cell to be put in to the table
-            TableCell tblCell1 = new TableCell();
-            TableCell tblCell2 = new TableCell();
-            TableCell tblCell3 = new TableCell();
+            foreach (string album in albumsList)
+            {
+                Image image = new Image();
+                image.ImageUrl = "~/resources/artwork/HotFuss.jpg";
+                //Initialize a row for the table
+                TableRow tblRow = new TableRow();
 
-            //Add data to the cell here
-            tblCell1.Text = album.ToString();
-            tblCell2.Text = album.ToString();
-            tblCell3.Controls.Add(image);            
+                //Creates a cell to be put in to the table
+                TableCell tblCell1 = new TableCell();
+                TableCell tblCell2 = new TableCell();
+                TableCell tblCell3 = new TableCell();
 
-            //Add the cell to the row
-            tblRow.Cells.Add(tblCell1);
-            tblRow.Cells.Add(tblCell2);
-            tblRow.Cells.Add(tblCell3);
+                //Add data to the cell here
+                tblCell1.Text = album.ToString();
+                tblCell2.Text = album.ToString();
+                tblCell3.Controls.Add(image);
 
-            //Add the row to the table
-            tblAlbums.Rows.Add(tblRow);
+                //Add the cell to the row
+                tblRow.Cells.Add(tblCell1);
+                tblRow.Cells.Add(tblCell2);
+                tblRow.Cells.Add(tblCell3);
+
+                //Add the row to the table
+                tblAlbums.Rows.Add(tblRow);
+            }
         }
     }
 }
